b8f46b9ade6ea049206331fce9cbb5b4056e32b4
vlad3k <vlad3k@bk.ru>
2019-09-26 17:03:04 +0500

    first steps in redux: install redux, reducer, action, store, dispatch, subscribe, bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, bindActionCreators } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducer';
+import * as actions from './actions';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducer);
+const { dispatch } = store;
+
+const { inc, dec, rnd } =
+  bindActionCreators(actions, dispatch);
+
+document
+  .getElementById('inc')
+  .addEventListener('click', inc);
+
+document
+  .getElementById('dec')
+  .addEventListener('click', dec);
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload =  Math.floor(Math.random() * 10);
+    rnd(payload);
+  });
+
+const update = () => {
+  document
+  .getElementById('counter')
+  .innerHTML = store.getState();
+};
+
+store.subscribe(update);
